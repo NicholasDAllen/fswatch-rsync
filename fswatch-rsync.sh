682ecdd66e522241816dce9d7631c72247a46463
Nick Allen <nallen@narrativescience.com>
2014-10-21 18:42:00 -0700

    Customizations
    Simplified command line args
    Don't rsync files as per .gitignore

--- fswatch-rsync.sh
+++ fswatch-rsync.sh
@@ -3,10 +3,13 @@
 # @author Clemens Westrup
 # @date 07.07.2014
 
-# This is a script to automatically synchronize a local project folder to a 
-# folder on a cluster server via a middle server. 
-# It watches the local folder for changes and recreates the local state on the 
+# This is a script to automatically synchronize a local project folder to a
+# folder on a remote server.
+# It watches the local folder for changes and recreates the local state on the
 # target machine as soon as a change is detected.
+#
+# Automatically ignores syncing files per .gitignore files if run
+# in root of git project
 
 # For setup and usage see README.md
 
@@ -15,23 +18,18 @@
 PROJECT="fswatch-rsync"
 VERSION="0.2.0"
 
-# Set up your path to fswatch here if you don't want to / can't add it 
-# globally to your PATH variable (default is "fswatch" when specified in PATH). 
-# e.g. FSWATCH_PATH="/Users/you/builds/fswatch/fswatch" 
-FSWATCH_PATH="/Users/cwestrup/extracted_source_builds/fswatch/fswatch"
-
 # Sync latency / speed in seconds
-LATENCY="3"
+LATENCY="1"
 
 # default server setup
-MIDDLE="james.hut.fi" # middle ssh server
-TARGET="triton.aalto.fi" # target ssh server
+TARGET="" # target ssh server
 
 # check color support
 colors=$(tput colors)
 if (($colors >= 8)); then
     red='\033[0;31m'
     green='\033[0;32m'
+    yellow='\033[0;33m'
     nocolor='\033[00m'
 else
   red=
@@ -40,37 +38,20 @@ else
 fi
 
 # Check compulsory arguments
-if [[ "$1" = "" || "$2" = "" || "$3" = "" ]]; then
+if [[ "$1" = "" || "$2" = "" ]]; then
   echo -e "${red}Error: $PROJECT takes 3 compulsory arguments.${nocolor}"
-  echo -n "Usage: fswatch-rsync.sh /local/path /targetserver/path ssh_user " 
-  echo    "[middleserver] [targetserver] [target_ssh_user]"
+  echo -n "Usage: fswatch-rsync.sh /local/path user@server:/path"
   exit
 else
   LOCAL_PATH="$1"
-  TARGET_PATH="$2"
-  SSH_USER="$3"
-fi
-
-# Check optional arguments
-if [[ "$4" != "" ]]; then
-  MIDDLE="$4"
-fi
-if [[ "$5" != "" ]]; then
-  TARGET="$5"
-fi
-if [[ "$6" != "" ]]; then
-  TARGET_SSH_USER="$6"
-else
-  TARGET_SSH_USER="$SSH_USER"
+  TARGET="$2"
 fi
 
 # Welcome
 echo      ""
-echo -e   "${green}Hei! This is $PROJECT v$VERSION.${nocolor}" 
+echo -e   "${green}$PROJECT v$VERSION.${nocolor}"
 echo      "Local source path:  \"$LOCAL_PATH\""
-echo      "Remote target path: \"$TARGET_PATH\""
-echo      "Via middle server:  \"$SSH_USER@$MIDDLE\""
-echo      "To target server:   \"$TARGET_SSH_USER@$TARGET\""
+echo      "Remote target: \"$TARGET\""
 echo      ""
 echo -n   "Performing initial complete synchronization "
 echo -n   "(Warning: Target directory will be overwritten "
@@ -80,22 +61,21 @@ echo      "with local version if differences occur)."
 read -n1 -r -p "Press any key to continue (or abort with Ctrl-C)... " key
 echo      ""
 echo -n   "Synchronizing... "
-rsync -avzr -q --delete --force --exclude=".*" \
--e "ssh $SSH_USER@$MIDDLE ssh" $LOCAL_PATH $TARGET_SSH_USER@$TARGET:$TARGET_PATH 
+rsync -avzr -q --delete --force --filter=':- .gitignore' $LOCAL_PATH $TARGET
 echo      "done."
 echo      ""
 
 # Watch for changes and sync (exclude hidden files)
 echo    "Watching for changes. Quit anytime with Ctrl-C."
-${FSWATCH_PATH} -0 -r -l $LATENCY $LOCAL_PATH --exclude="/\.[^/]*$" \
-| while read -d "" event 
-  do 
-    echo $event > .tmp_files
-    echo -en "${green}" `date` "${nocolor}\"$event\" changed. Synchronizing... "
-    rsync -avzr -q --delete --force \
-    --include-from=.tmp_files \
-    -e "ssh $SSH_USER@$MIDDLE ssh" \
-    $LOCAL_PATH $TARGET_SSH_USER@$TARGET:$TARGET_PATH 
-  echo "done."
-    rm -rf .tmp_files
+fswatch -0 -r -l $LATENCY $LOCAL_PATH --exclude="/\.[^/]*$" | while read -d "" event
+  do
+    git check-ignore $event -q
+    ignore=$?
+    if [ "${ignore}" == "0" ]; then
+      echo -e "${yellow}" `date` "${nocolor}\"${event}\" ignored"
+    else
+      echo -en "${green}" `date` "${nocolor}\"$event\" changed. Synchronizing... "
+      rsync -avz -q --delete --force $LOCAL_PATH $TARGET
+      echo "done."
+    fi
   done
